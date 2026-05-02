43655d4475415555ddb4c1dcc7789e2c2aa797f3
Ardneb <ben@greiveldinger.eu>
2026-05-02 17:03:47 +0000

    Corrected python code

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -2,5 +2,6 @@
 def print_last_digit(number):
     last_digit = number % 10
     if number < 0 and last_digit != 0:
-        last_digit - 10
-     print(last_digit)
+        last_digit -= 10
+    print(last_digit, end="")
+    return last_digit
