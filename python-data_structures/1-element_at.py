22dceb72c9e5f2e365be494a7405d1fff0e70b30
Ardneb <ben@greiveldinger.eu>
2026-05-06 16:40:23 +0000

    Corrected 1-element_at.py

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -2,6 +2,6 @@
 def element_at(my_list, idx):
     if __name__ == "__main__":
         if 0 <= idx < len(my_list):
-            print('Element at index {} is {}'.format(idx, my_list[idx]))
+            print('Element at index {:d} is {:d}'.format(idx, my_list[idx]))
         else:
-            print('Element at index {} is None'.format(idx))
+            print('Element at index {:d} is None'.format(idx))
