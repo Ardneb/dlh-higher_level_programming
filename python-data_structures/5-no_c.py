81525a9b70a8164d94abe6e38b2be200b5423df2
Ardneb <ben@greiveldinger.eu>
2026-05-08 17:03:22 +0000

    Corrected 5-no_c.py

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -4,4 +4,4 @@ def no_c(my_string):
     for i in range(len(my_string)):
         if my_string[i].lower() != "c":
             new_string = new_string + my_string[i]
-        return new_string
+    return new_string
