a8dad0e6669a0da9d4f119954ac7a891ff475963
Ardneb <ben@greiveldinger.eu>
2026-05-08 17:00:05 +0000

    Added 5-no_c.py and updated README.md

---
+++ python-data_structures/5-no_c.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    new_string = ""
+    for i in reange(len(my_string)):
+        if my_string[i].lower() != "c":
+            new_string = new_string + my_string[i]
+        return new_string
