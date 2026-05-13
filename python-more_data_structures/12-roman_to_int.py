d41eec2ff79ad03bd2f79424a15c5ce7e6e28cef
Ardneb <ben@greiveldinger.eu>
2026-05-13 17:10:13 +0000

    Corrected 12-roman_to_int.py

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,7 @@ def roman_to_int(roman_string):
     roman_dict = {"M": 1000, "C": 100, "D": 500, "X": 10, "L": 50, "I": 1, "V": 5}
     if roman_string is not None and type(roman_string) == str:
         number = 0
-        for i in range(len(roman_string)):
+        for i in range(0, len(roman_string)):
             if i == len(roman_string) - 1:
                 number = number + roman_dict[roman_string[i]]
             elif roman_dict[roman_string[i]] > roman_dict[roman_string[i + 1]]:
@@ -11,4 +11,3 @@ def roman_to_int(roman_string):
             elif roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]:
                 number = number - roman_dict[roman_string[i]]
         return number
-
