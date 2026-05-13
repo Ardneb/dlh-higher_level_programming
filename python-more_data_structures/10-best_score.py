9ecefee7929c52c8b6898c7963a39a167143378c
Ardneb <ben@greiveldinger.eu>
2026-05-13 11:56:30 +0000

    Corrected 10-best_score.py

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if  a_dictionary is not None and len(a_dictionary) > 0:
-        a_dictionary = {key: value for key, value in sorted(a_dictionary.items(), key=lambda item: item[1], reverse=True)}
-        return next(iter(a_dictionary))
+    if a_dictionary is not None and len(a_dictionary) > 0:
+        a_dictionary = {key: value for key, value in sorted(a_dictionary.items(), key=lambda item: item[1])}
+        return list(a_dictionary.keys())[-1]
     else:
         None
