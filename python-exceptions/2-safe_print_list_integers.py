0d22147fc6bf3116d817c5c9d55cf4dc152bc3a0
Ardneb <ben@greiveldinger.eu>
2026-05-18 14:47:16 +0000

    Added 2-safe_print_list_integer.py

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    nb = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            nb = nb + 1
+        except IndexError:
+            print("x bigger than length of list")
+        except ValueError:
+            pass
+        except TypeError:
+            pass
+    print()
+    return nb
