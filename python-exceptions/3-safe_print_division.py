0968e9846dd218eb02eb819fac0e3f483f25b0b6
Ardneb <ben@greiveldinger.eu>
2026-05-18 15:15:04 +0000

    Corrected 3-safe_print_division.py

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -2,7 +2,7 @@
 def safe_print_division(a, b):
     try:
         result = a / b
-    except:
+    except ZeroDivisionError:
         None
     finally:
         print("Inside result: {:d}".format(result))
