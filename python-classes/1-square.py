2112560aeba89c8399ec5b96fc386ce103c20566
Ardneb <ben@greiveldinger.eu>
2026-05-24 11:20:42 +0000

    Added 1-square.py and updated README.md

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+#!/usr/bin/python3
+"""This module creates an class Square that defines a square"""
+
+
+class Square:
+    """This class defines a square"""
+    def __init__(self, size):
+        self.__size = size
--- python-classes/README.md
+++ python-classes/README.md
@@ -1,2 +1,4 @@
 ## 0-square.py
 Write an empty class Square that defines a square
+## 1-square.py
+Write a class Square that defines a square by private instance attribute size
