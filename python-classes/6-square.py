8324785d8f2550494b4699886b0c3594ca41c3ff
Ardneb <ben@greiveldinger.eu>
2026-05-24 13:49:05 +0000

    Corrected 6-square.py

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -35,9 +35,9 @@ class Square:
         """Check if position is a tuple of 2 positive integers"""
 
         if (not isinstance(value, tuple) or
+                len(value) != 2 or
                 not isinstance(value[0], int) or
                 not isinstance(value[1], int) or
-                len(value) != 2 or
                 value[0] < 0 or
                 value[1] < 0):
             raise TypeError("position must be a tuple of 2 positive integers")
