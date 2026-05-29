8f5bebf604fc080e7fc5af1260ff975d7c94b6d5
Ardneb <ben@greiveldinger.eu>
2026-05-29 17:37:49 +0000

    Corrected 5-rectangle.py

--- python-more_classes/5-rectangle.py
+++ python-more_classes/5-rectangle.py
@@ -66,7 +66,6 @@ class Rectangle:
         """Returns the string representation of the rectangle"""
         return "Rectangle({}, {})".format(self.width, self.height)
         
-        
     def __del__(self):
         """Deletes the rectangle"""
         print("Bye rectangle...")
