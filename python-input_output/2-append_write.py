3129e81f8c2c433382f9f117c01bb44ecf7ae69a
Ardneb <ben@greiveldinger.eu>
2026-05-30 09:43:33 +0000

    Corrected 2-append_write.py

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -7,5 +7,5 @@ def append_write(filename="", text=""):
     and returns the number of characters added
     """
     with open(filename, mode="a", encoding="utf-8") as myFile:
-        myfile.write(text)
+        myFile.write(text)
         return len(text)
