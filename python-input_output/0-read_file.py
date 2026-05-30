f994e83974ce6e2ae422b1fd0296fa3ed66e39bc
Ardneb <ben@greiveldinger.eu>
2026-05-30 09:17:46 +0000

    Corrected 0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Reading text and printing it"""
+
+
 def read_file(filename=""):
     """function reads a text file and prints it"""
     with open(filename, encoding="utf-8") as myFile:
