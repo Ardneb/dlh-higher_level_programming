a3cccd1fb0bd54093a99268948414e7f25ca4308
Ardneb <ben@greiveldinger.eu>
2026-05-30 12:33:21 +0000

    Added 11-student.py

---
+++ python-input_output/11-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Student to JSON"""
+
+
+class Student:
+    """This class defines a student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Instantiation of Student with public instances"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieve dictionnary representation of a student instance"""
+        new_dict = {}
+        if attrs is None:
+            return self.__dict__
+        else:
+            for i in self.__dict__:
+                if i in attrs:
+                    new_dict[i] = self.__dict__[i]
+            return new_dict
+
+    def reload_from_json(self, json):
+        """Replaces all attributes of the Student instance"""
+        for key, value in json.items():
+            setattr(self, key, value)
