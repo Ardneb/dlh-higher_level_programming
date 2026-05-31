8a99085a6842b1f5471e9be5b7672ba84538e32e
Ardneb <ben@greiveldinger.eu>
2026-05-31 17:16:32 +0200

    Corrected task_00_basic_serialization.py

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 """Basic Serialization"""
-import pickle
+import json
 
 
 def serialize_and_save_to_file(data, filename):
     """Serialize a Python dictionary to a JSON file"""
-    with open(filename, "wb") as myFile:
-        pickle.dump(data, myFile)
+    with open(filename, mode="w") as myFile:
+        json.dump(data, myFile)
     pass
 
 
 def load_and_deserialize(filename):
     """Deserialize the JSON file to recreate the Python Dictionary"""
     with open(filename, "rb") as myFile:
-        return pickle.load(myFile)
+        return json.load(myFile)
     pass
